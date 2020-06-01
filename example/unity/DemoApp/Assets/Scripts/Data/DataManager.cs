5ac38e80833d107ea683d56c65a38a9a12f05333
10dimensions <vishal.145@hotmail.com>
2020-06-01 20:00:44 +0530

    dynamic parsing

---
+++ example/unity/DemoApp/Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class DataManager: MonoBehaviour
+{   
+    public JSONNode JsonData;
+    public JSONNode DeviceList;
+
+    [SerializeField] private GameObject IotI;
+
+    void Awake()
+    {
+        JsonData = JSON.Parse( LoadResourceTextfile("state.json") );
+        DeviceList = JsonData["response"]["device_groups"][0]["device_sensors"];
+
+        IotI.SetActive(true);
+
+    }
+
+    public string LoadResourceTextfile(string path)
+    {
+        string filePath = "" + path.Replace(".json", "");
+        TextAsset targetFile = Resources.Load<TextAsset>(filePath);
+        return targetFile.text;
+    }
+
+    public float GetValueFromSensor(int _idx)
+    {   
+        string _type = DeviceList[_idx]["device_type"];
+        return float.Parse(DeviceList[_idx]["device_value"]);
+    }
+
+    public string GetTypeOfSensor(int _idx)
+    {
+        return DeviceList[_idx]["device_type"];
+    }
+
+    public void SetValueFromSensor()
+    {
+
+    }
+
+}
---
+++ example/unity/DemoApp/Assets/Scripts/Rotate2.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Rotate2 : MonoBehaviour, IEventSystemHandler
+{   
+    [SerializeField] private GameObject Devices;
+    [SerializeField] private float[] DeviceValues;
+
+    [SerializeField] private DataManager DataI;
+
+    void Awake()
+    {
+        SetUpScene();
+    }
+
+    private void SetUpScene()
+    {
+        Devices = GameObject.FindWithTag("Devices");
+        DeviceValues = new float[Devices.transform.childCount - 1];
+
+        //for(int i=0; i<_deviceList.Count; i++)
+        for(int i=3; i<5; i++)
+        {   
+            Devices.transform.GetChild(i).name = DataI.DeviceList[i]["bridge_name"];
+            DeviceValues[i] = DataI.GetValueFromSensor(i);
+        }
+    }
+
+    private void SetDeviceValue(int _id)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        RaycastObjects();
+
+        Devices.transform.GetChild(4).transform.Rotate(new Vector3(0,DeviceValues[4],0) * Time.deltaTime * 1000);   
+
+        if(DeviceValues[3] == 1f)
+        {
+            Devices.transform.GetChild(3).GetComponent<Light>().range = DeviceValues[4]*80f;
+        }
+        else
+        {
+            Devices.transform.GetChild(3).GetComponent<Light>().range = 0f;
+        }
+    }
+
+
+    private void RaycastObjects()
+    {   
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            var hit = new RaycastHit();
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {  
+                // This method is used to send data to Flutter
+                UnityMessageManager.Instance.SendMessageToFlutter(DataI.DeviceList);
+                
+                Debug.Log("ping " + hit.transform.name);
+            }
+        }
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {   
+            if (Input.GetTouch(i).phase.Equals(TouchPhase.Began))
+            {
+                var hit = new RaycastHit();
+
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+
+                if (Physics.Raycast(ray, out hit))
+                {  
+                    // This method is used to send data to Flutter
+                    UnityMessageManager.Instance.SendMessageToFlutter("The cube feels touched.");
+                    Debug.Log(hit);
+                }
+            }
+        }
+    }
+
+    // This method is called from Flutter
+    public void SetRotationSpeed(String message)
+    {
+        float value = float.Parse(message);
+        //RotateValue = value;
+    }
+
+    // This method is called from Unity
+    public void SwitchLightOn(String message)
+    {
+        bool lightOn = ( message == "true");
+
+        DeviceValues[3] = lightOn==true?1:0;
+    }
+
+    // This method is called from Unity
+    public void SetRotationVelocity(float message)
+    {
+        DeviceValues[4] = message;
+        DataI.DeviceList[4]["device_value"] = DeviceValues[4].ToString();
+
+        UnityMessageManager.Instance.SendMessageToFlutter(DeviceValues[4].ToString());
+    }
+
+    // This method is called from Unity
+    public void TurnLightOn(bool message)
+    {
+        DeviceValues[3] = message==true?1f:0f;
+        DataI.DeviceList[3]["device_value"] = DeviceValues[3].ToString();  
+
+        UnityMessageManager.Instance.SendMessageToFlutter(DeviceValues[3].ToString());
+    }
+
+    private void OnApplicationExit()
+    {
+        //Debug log datai jsondata
+    }
+
+}
